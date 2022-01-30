fe118ae6922ee2b969d39dde67a0af5a5d0c5df3
nicholaschangIT <nicholasc001@hotmail.com>
2022-01-30 19:43:28 +0000

    example terraform pipline

--- example.tf
+++ example.tf
@@ -11,16 +11,17 @@ terraform {
  
 data "azurerm_client_config" "current" {}
  
-resource "azurerm_resource_group" "tamopsrg" {
-  name     = "tamops-tf"
-  location = "eastus2"
+resource "azurerm_resource_group" "terraformrg" {
+  name     = "terraformstf"
+  location = "UKSouth"
 }
  
-resource "azurerm_storage_account" "tamopssa" {
-  name                     = "tamopssatf"
+resource "azurerm_storage_account" "teststorage" {
+  name                     = "testtf"
   resource_group_name      = azurerm_resource_group.tamopsrg.name
   location                 = azurerm_resource_group.tamopsrg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
 
